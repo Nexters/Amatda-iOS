b62bdb3a0982ba22509662bcebe664a521b7f5db
JHH <wjdgo813@naver.com>
2019-01-06 16:48:15 +0900

    아맞다!

---
+++ Amatda-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amatda-iOS
+//
+//  Created by Haehyeon Jeong on 06/01/2019.
+//  Copyright © 2019 JHH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Amatda-iOSTests/Amatda_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Amatda_iOSTests.swift
+//  Amatda-iOSTests
+//
+//  Created by Haehyeon Jeong on 06/01/2019.
+//  Copyright © 2019 JHH. All rights reserved.
+//
+
+import XCTest
+@testable import Amatda_iOS
+
+class Amatda_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
